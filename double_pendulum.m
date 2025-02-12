5ced88d1618e4bb3fd59c888b5a381dd9c4692f8
Aidan Maldonado <aidanmaldonado@eduroam-169-233-229-168.ucsc.edu>
2025-02-12 12:18:12 -0800

    Add Runge-Kutta method

--- double_pendulum.m
+++ double_pendulum.m
@@ -5,7 +5,7 @@ g  = 9.81;            % gravity (m/s^2)
 m1 = 1;               % mass 1 (kg)
 m2 = 1;               % mass 2 (kg)
 L1 = 1;               % pendulum arm 1 length (m)
-L2 = [0.25, 0.5];     % pendulum arm 2 length (m) - [1st simulation, 2nd simulation]
+L2 = [.25, .50];      % pendulum arm 2 length (m) - [1st simulation, 2nd simulation]
 
 % Initial Conditions
 theta1_init = 1 / (3*pi);  % initial angle of arm 1 from equilibrium (radians)
@@ -15,7 +15,8 @@ theta2_dot  = 0;           % initial angular velocity of arm 2 (rad/s)
 
 % Simulation Settings
 t_step    = [0.1, .01];   % simulation timestamp precision (s/step) - [1st simulation, 2nd simulation]
-duration  = 5;            % simulation duration (s) [** At around 30s it devolves into chaos **]
+duration  = 5;            % simulation duration (s)
+
 
 % % Run Simulations
 
@@ -28,13 +29,13 @@ xlim([(-L1 - max_L2) * 1.1, (L1 + max_L2) * 1.1])
 ylim([(-L1 - max_L2) * 1.1, (L1 + max_L2) * 1.1])
 title("Double Pendulum")
 
+% Initialize Pendulums & Simulate
 euler_method_1 = simulation(@euler_method, g, m1, m2, L1, L2(1), theta1_init, theta1_dot, theta2_init, theta2_dot, t_step(1), duration);
-euler_method_1.run() % [**** Seems wrong :') Euler 1 actually seems to work fine if it uses the smaller t_step (2) ****]
+euler_method_1.run()
 euler_method_1.animate("Euler Method 1")
 
-
 euler_method_2 = simulation(@euler_method, g, m1, m2, L1, L2(2), theta1_init, theta1_dot, theta2_init, theta2_dot, t_step(2), duration);
-euler_method_2.run() % Seems alright?
+euler_method_2.run()
 euler_method_2.animate("Euler Method 2")
 
 trapezoid_method_1 = simulation(@trapezoid_method, g, m1, m2, L1, L2(1), theta1_init, theta1_dot, theta2_init, theta2_dot, t_step(1), duration);
@@ -44,11 +45,13 @@ trapezoid_method_1.animate("Trapezoid Method 1")
 trapezoid_method_2 = simulation(@trapezoid_method, g, m1, m2, L1, L2(2), theta1_init, theta1_dot, theta2_init, theta2_dot, t_step(2), duration);
 trapezoid_method_2.run()
 trapezoid_method_2.animate("Trapezoid Method 2")
-% 
-% runge_kutta_method_1 = simulation(@runge_kutta_method, g, m1, m2, L1, L2(1), theta1_init, theta1_dot, theta2_init, theta2_dot, t_step(1), duration);
-% runge_kutta_method_1.run()
-% 
-% runge_kutta_method_2 = simulation(@runge_kutta_method, g, m1, m2, L1, L2(2), theta1_init, theta1_dot, theta2_init, theta2_dot, t_step(2), duration);
-% runge_kutta_method_2.run()
+
+runge_kutta_method_1 = simulation(@runge_kutta_method, g, m1, m2, L1, L2(1), theta1_init, theta1_dot, theta2_init, theta2_dot, t_step(1), duration);
+runge_kutta_method_1.run()
+runge_kutta_method_1.animate("Runge-Kutta Method 1")
+
+runge_kutta_method_2 = simulation(@runge_kutta_method, g, m1, m2, L1, L2(2), theta1_init, theta1_dot, theta2_init, theta2_dot, t_step(2), duration);
+runge_kutta_method_2.run()
+runge_kutta_method_2.animate("Runge-Kutta Method 2")
 
 hold off;
--- runge_kutta_method.m
+++ runge_kutta_method.m
@@ -1,5 +1,5 @@
 % Calculate the angular mechanics of the system at the next state
-function next_state =  runge_kutta_method(current_state, g, m1, m2, L1, L2, step)
+function next_state =  runge_kutta(current_state, g, m1, m2, L1, L2, step)
     
     % Unpack current state values
     theta1     = current_state.theta1;
@@ -7,11 +7,35 @@ function next_state =  runge_kutta_method(current_state, g, m1, m2, L1, L2, step
     theta2     = current_state.theta2;
     theta2_dot = current_state.theta2_dot;
 
+    % Calculate k1 values
+    k1_theta1     = theta1_dot;
+    k1_theta1_dot = M1AA(g, m1, m2, theta1, theta1_dot, theta2, theta2_dot, L1, L2);
+    k1_theta2     = theta2_dot;
+    k1_theta2_dot = M2AA(g, m1, m2, theta1, theta1_dot, theta2, theta2_dot, L1, L2);
+
+    % Calculate k2 values
+    k2_theta1     = theta1_dot + (1/2) * step * k1_theta1_dot;
+    k2_theta1_dot = M1AA(g, m1, m2, theta1 + (step/2) * k1_theta1, theta1_dot + (1/2) * step * k1_theta1_dot, theta2 + (1/2) * step * k1_theta2, theta2_dot + (1/2) * step * k1_theta2_dot, L1, L2);
+    k2_theta2     = theta2_dot + (1/2) * step * k1_theta2_dot;
+    k2_theta2_dot = M2AA(g, m1, m2, theta1 + (1/2) * step * k1_theta1, theta1_dot + (1/2) * step * k1_theta1_dot, theta2 + (1/2) * step * k1_theta2, theta2_dot + (1/2) * step * k1_theta2_dot, L1, L2);
+
+    % Calculate k3 values
+    k3_theta1     = theta1_dot + (1/2) * step * k2_theta1_dot;
+    k3_theta1_dot = M1AA(g, m1, m2, theta1 + (1/2) * step * k2_theta1, theta1_dot + (1/2) * step * k2_theta1_dot, theta2 + (1/2) * step * k2_theta2, theta2_dot + (1/2) * step * k2_theta2_dot, L1, L2);
+    k3_theta2     = theta2_dot + (1/2) * step * k2_theta2_dot;
+    k3_theta2_dot = M2AA(g, m1, m2, theta1 + (1/2) * step * k2_theta1, theta1_dot + (1/2) * step * k2_theta1_dot, theta2 + (1/2) * step * k2_theta2, theta2_dot + (1/2) * step * k2_theta2_dot, L1, L2);
+
+    % Calculate k4 values
+    k4_theta1     = theta1_dot + step * k3_theta1_dot;
+    k4_theta1_dot = M1AA(g, m1, m2, theta1 + step * k3_theta1, theta1_dot + step * k3_theta1_dot, theta2 + step * k3_theta2, theta2_dot + step * k3_theta2_dot, L1, L2);
+    k4_theta2     = theta2_dot + step * k3_theta2_dot;
+    k4_theta2_dot = M2AA(g, m1, m2, theta1 + step * k3_theta1, theta1_dot + step * k3_theta1_dot, theta2 + step * k3_theta2, theta2_dot + step * k3_theta2_dot, L1, L2);
+
     % Calculate values of next state using Runge-Kutta's Method
-    new_theta1     = 0; % To-do
-    new_theta1_dot = 0; % To-do
-    new_theta2     = 0; % To-do
-    new_theta2_dot = 0; % To-do
+    new_theta1     = theta1 + (1/6) * step * (k1_theta1 + 2 * k2_theta1 + 2 * k3_theta1 + k4_theta1);
+    new_theta1_dot = theta1_dot + (1/6) * step * (k1_theta1_dot + 2*k2_theta1_dot + 2 * k3_theta1_dot + k4_theta1_dot);
+    new_theta2     = theta2 + (1/6) * step * (k1_theta2 + 2 * k2_theta2 + 2 * k3_theta2 + k4_theta2);
+    new_theta2_dot = theta2_dot + (1/6) * step * (k1_theta2_dot + 2 * k2_theta2_dot + 2 * k3_theta2_dot + k4_theta2_dot);
     
     % Package and return
     next_state = state(new_theta1, new_theta1_dot, new_theta2, new_theta2_dot);
